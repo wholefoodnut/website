03fea6557fddc782dd3b8ae59249660633f847a3
d3nise <denise.thuyvy@gmail.com>
2018-04-13 13:14:48 -0700

    old wfn js

--- js/input.js
+++ js/input.js
@@ -1,77 +1,92 @@
-var data = [];
+var food = [];
+
+
+/*
+    https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+*/
+
+printJson();
+function printJson(){
+    var food = '{"foodSwap":[' +
+    '{"macro":"1","foodItem":"Avo Oil" },' +
+    '{"macro":"2","foodItem":"Quinoa" },' +
+    '{"macro":"3","foodItem":"Tempeh" }]}';
+    
+    var testing = '<strong>JSON Test: </strong> ';
+    document.getElementById('jsonTest').innerHTML = testing;
+    
+    //var obj = JSON.parse(food);
+    var myJSON = JSON.stringify(food);
+    document.getElementById("jsonTestPP").innerHTML = myJSON;
+/*    ( 
+
+        /*obj.foodSwap[1].macro + " " +
+        obj.foodSwap[1].foodItem
+          );
+    // /console.log(obj.foodSwap[1].foodItem);
+  */
+
+
+}
+var mac = [];
 function getFoodinput(){
     var getitem = document.getElementById("food1").value;
     var getMac = document.getElementById("macro1").value;
     // alert(+getMac+ " Food entered: "  +getitem);
 
     //Print user input        
-    data.push(getMac);
-    data.push(getitem);
+    //food.push(getitem);
+    mac.push(getMac);
 
     /*Print array elements*/     
-    var newString = ' ';
-    for(var i = 0; i < data.length; i++){  
-        newString.concat(data[i] + ' ' );
-        newString.concat(data[i] + ' <br/> ' );
+    var finput = ' ';
+    for(var i = 0; i < food.length; i++){  
+        finput.concat(food[i] + ' ' );
     }
+
+    var minput = ' ';
+    for(var i = 0; i < mac.length; i++){  
+        minput.concat(mac[i] + ' ' );
+    }
+
     //Prints before array
     var text = '<strong>Your food item: </strong> ';
     document.getElementById('foodText').innerHTML = text;
     //Prints data pushed into data array 
-    document.getElementById('printFood').innerHTML = data.toString();
-
+    document.getElementById('foodInput').innerHTML = food.toString();
+    document.getElementById('macroInput').innerHTML = mac.toString();
 
     //Checking array for food marco # to switch 
-    for(var i=0; i<data.length;i++){
-        if (data[i] == 0){
-            data.pop(getMac);
-            data.push('<br/> You did not select anything ');
+    for(var i=0; i<mac.length;i++){
+        if (mac[i] == 0){
+            mac.pop(getMac);
+            mac.push('<br/> You did not select anything ');
         }
-        if (data[i] == 1){
-            data.pop(getMac);
-            data.push('<br/> Avocado Oil, Cocunut Oil, Olive Oil\n')
+        if (mac[i] == 1){
+            mac.pop(getMac);
+            mac.push('<br/> Avocado Oil, Cocunut Oil, Olive Oil\n')
         }
-        if (data[i] == 2){
-            data.pop(getMac);
-            data.push('<br/> Quinoa, Amaranth, Teff\n');
+        if (mac[i] == 2){
+            mac.pop(getMac);
+            mac.push('<br/> Quinoa, Amaranth, Teff\n');
         }
-        if (data[i] == 3){
-            data.pop(getMac);
-            data.push('<br/>Tempeh, tofu, soybeans\n' );    
+        if (mac[i] == 3){
+            mac.pop(getMac);
+            mac.push('<br/>Tempeh, tofu, soybeans\n' );    
         }
         
     }
- 
     var new2String = ' ';
-    for(var i = 0; i < data.length; i++){
-        new2String.concat(data[i] + ' ' );
+    for(var i = 0; i < mac.length; i++){
+        new2String.concat(mac[i] + ' ' );
     }
     var text = '<strong>Your food swap recommendation: </strong>';
     document.getElementById('foodSwapText').innerHTML = text;
     //Prints data pushed into data array 
-    document.getElementById('foodSwap').innerHTML = data.toString();
+    document.getElementById('foodSwap').innerHTML = mac.toString();
 }
 
 
-/*
-class Ingredient {
-    constructor() {
-      this.name = "User Ingredient";
-    }
-  }
-  
-  var food = new Ingredient();
-  console.log(food.name);
-
-  var obj = {
-    foo() {
-      return getFoodinput();
-    }
-  }
-  
-  console.log(obj.foo());
-  // expected output: "bar"
-*/
 /*
  DENISE THUY VY NGUYEN
  3/21/2018
